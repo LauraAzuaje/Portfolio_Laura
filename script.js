3ae0f19df2bf77b3dbc1dc3b5d5c23530f0fee52
unknown <lyam35,25@gmail.com>
2021-12-20 23:09:59 -0500

    upload files

---
+++ script.js
@@ -0,0 +1,16 @@
+// static menu//
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // activation menu
+    $('.btn-menu').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.btn-menu i').toggleClass("active");
+    });
+});
