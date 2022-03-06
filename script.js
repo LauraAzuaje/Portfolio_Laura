77eb7fe8abb22f46c009fc5f70ec81b61d1a3625
LauraAzuaje <lyam35.25@gmail.com>
2022-03-06 15:39:17 -0500

    form

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ $(document).ready(function(){
   
     });
 });
+
+//Form submit
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#emailSubmit')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:azuajemarcanolaura@gmail.com?subject= nombre ${form.get('name')}  correo ${form.get('email')}&body= ${form.get('message')}`)
+  $buttonMailto.click()
+}
